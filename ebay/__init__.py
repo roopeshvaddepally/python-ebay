3728ceb7b9dd2f19b04b8dd1cb3e82639c73392e
Roopesh Vaddepally <roopeshvaddepally@gmail.com>
2011-05-27 06:16:57 -0700

    add ebay object to configure the calls, and enable dynamic api invocation
    The documentation is added, implementation is yet to be done. This class would dynamically invoke the methods in package, while passing some boilerplate arguments, like certificates and ids.

--- ebay/__init__.py
+++ ebay/__init__.py
@@ -1,1 +1,21 @@
 def configure(*args, **kwargs): pass
+
+class EBay(object):
+    """An object that helps to wrap around raw api calls.
+
+    EBay object initializes an object which take the dev keys and
+    certificates and configures the parameters being passed to the
+    url calls.
+    
+    Instead of keys, the path to file can be given. By default, the
+    object reads from the config.ini file from current working directory.
+    ::
+    
+    >>> ebay = EBay() # reads from config.ini file.
+    >>> ebay.finding.getVersion() # calls finding.py/getVersion with
+                                  # appropriate parameters.
+    """
+    def __init__(self, dev=None, app=None, cert=None, config="config.ini"):
+        ## read the config file and load the dev, app, cert variables.
+        ## override the variables from the paramters passed in.
+        
