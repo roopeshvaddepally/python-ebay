188c3663329e00f84912d838a9c802daa949366f
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-05-29 16:34:21 -0700

    updated shopping.py

--- ebay/shopping.py
+++ ebay/shopping.py
@@ -1,16 +1,16 @@
-import requests
 from ConfigParser import ConfigParser
+import requests
 from utils import relative
 
 # Item Search
 def FindProducts(encoding, query, available_items, max_entries):
-
     response = get_response(
                     callname= FindProducts.__name__,  
                     responseencoding=encoding,
                     QueryKeywords=query,
                     AvailableItemsOnly=available_items,
-                    MaxEntries=max_entries)
+                    MaxEntries=max_entries
+                    )
     
     return response.content
     
@@ -38,19 +38,16 @@ def GeteBayTime(): pass
 
 
 def get_response(*args, **kwargs):
+    endpoint = "http://open.api.sandbox.ebay.com/shopping"
+    
     config = ConfigParser()
     config.read(relative("..", "config", "config.ini"))
     
     app_id = config.get("keys", "app_name")
     site_id = config.get("call", "siteid")
     version = config.get("call", "compatibility_level")
-    
-    endpoint = "http://open.api.sandbox.ebay.com/shopping"
 
-    d=dict(appid = app_id,
-           siteid = site_id,
-           version = version)
-    
+    d=dict(appid = app_id, siteid = site_id, version = version)
     d.update(kwargs)
     
     return requests.get(endpoint, params=d)
