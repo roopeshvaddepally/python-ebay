883ae6db83ec02e1492309486d9ce04db35d5c71
Roopesh Vaddepally <roopeshvaddepally@gmail.com>
2011-05-29 16:40:05 -0700

    removed unnecessary utility functions

--- ebay/utils.py
+++ ebay/utils.py
@@ -1,25 +1,4 @@
-from ConfigParser import ConfigParser
 from os.path import join, dirname, abspath
-
-def configure(*args, **kwargs):
-    config = ConfigParser()
-    config.read("config.ini")
-
-    dev_name = config.get("keys", "dev_name")
-    app_name = config.get("keys", "app_name")
-    cert_name = config.get("keys", "cert_name")
-    server_url = config.get("server", "url")
-    server_dir = config.get("server", "dir")
-    token = config.get("auth", "token")
-    siteid = config.get("call", "siteid")
-    compatiblity_level = config.get("call", "compatibility_level")
-
-    d = dict(dev_id=dev_id, app_id=app_id, cert_id=cert_id,
-             server_url=server_url, server_dir=server_dir,
-             token=token,siteid=siteid, 
-             compatiblity_level=compatiblity_level)
-    d.update(kwargs)
-    return d
 
 def relative(*paths):
     return join(dirname(abspath(__file__)), *paths)
