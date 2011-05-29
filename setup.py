4333480055f3c05c74f03977215a01e56b7b16fa
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-05-29 16:23:56 -0700

    updated setup.py with requests

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(name='python-ebay',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+          "requests"
       ],
       entry_points="""
       # -*- Entry points: -*-
