982a0780caf858b931b6d537625ae452d5e724b9
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-06-04 17:19:55 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,27 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
-
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    
 setup(name='python-ebay',
-      version=version,
-      description="Python wrapper for eBay API",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      version="0.1",
+      description="Python Wrapper for eBay API",
+      long_description=read('README.md'),
+      classifiers = [
+        "Development Status :: 4 - Beta",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "License :: OSI Approved :: Apache Software License",
+    ],
       keywords='ebay api wrapper',
-      author='Roopesh Vaddepally',
-      author_email='roopeshvaddepally@gmail.com',
-      url='',
-      license='',
+      author='Utkars Sengar, Roopesh Vaddepally',
+      author_email='utkarsh2012@gmail.com, roopeshvaddepally@gmail.com',
+      url='https://github.com/roopeshvaddepally/python-ebay',
+      license='Apache Software License',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
