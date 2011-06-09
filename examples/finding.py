e81907d0a43c279fd38be8cc6b6d88d1f84391db
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-06-08 17:21:54 -0700

    added test file

--- examples/finding.py
+++ examples/finding.py
@@ -1,10 +1,9 @@
 from ebay.finding import getSearchKeywordsRecommendation, findItemsByKeywords, findItemsByCategory, findItemsAdvanced, findItemsByProduct, findItemsIneBayStores, getHistograms
 
-#print getSearchKeywordsRecommendation(encoding="XML", keywords="acordian")
-#print findItemsByKeywords(keywords="ipod")
-#print findItemsByCategory(categoryId="123")
-#print findItemsAdvanced()
-#print findItemsByProduct(productId="123")
-#print findItemsIneBayStores()
-
+print getSearchKeywordsRecommendation(encoding="XML", keywords="acordian")
+print findItemsByKeywords(keywords="ipod")
+print findItemsByCategory(categoryId="123")
+print findItemsAdvanced()
+print findItemsByProduct(productId="123")
+print findItemsIneBayStores()
 print getHistograms(categoryId="12")
