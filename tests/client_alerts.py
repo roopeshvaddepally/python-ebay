697796421ed1366780fccdf7ddb7ad90ac86a262
Utkarsh Sengar <utkarsh2012@gmail.com>
2011-06-09 23:08:25 -0700

    update config.ini.example

---
+++ tests/client_alerts.py
@@ -0,0 +1,13 @@
+import unittest
+from lxml import etree
+
+from ebay.client_alerts import *
+
+
+class TestClientAlertsApi(unittest.TestCase):
+    def test_GetPublicAlerts(self):pass
+    def test_GetUserAlerts(self):pass
+    def test_Login(self):pass
+    def test_Logout(self):pass
+if __name__ == '__main__':
+    unittest.main()
