bf1a6afc2d539f8593e14551ec1654945dad7de8
Eike Welk <eike.welk@gmx.net>
2013-02-03 03:51:52 +0100

    `test_finding.py` now runs without error.
    Only thing left out is: Get a valid product ID for an iPod (eg. Nano).

--- tests/test_finding.py
+++ tests/test_finding.py
@@ -3,15 +3,16 @@ from lxml import etree
 
 from ebay.finding import *
 
-encoding = "XML"
+encoding = "XML" #default "JSON": Output encoding
 keywords = "ipod"
-categoryId = "12"
-productId = "1"
-storeName = "store1"
+#Get category IDs with function: `ebay.shopping.GetCategoryInfo`
+categoryId = "73839" #iPods & MP3 Players
+productId = "123" #TODO: get product ID for iPod. Each product eg. "iPod Nano" has a unique ID.
+storeName = "Fab Finds 4 U"
 #This information is encoded in URLs so the affiliate can get his commission. 
 affiliate = {"networkId":"9", "trackingId":"1234567890"}
-buyerPostalCode = "95112"
-paginationInput = {"entriesPerPage": "5", "pageNumber" : "1"}
+buyerPostalCode = "10027" #central New York City, USA
+paginationInput = {"entriesPerPage": "10", "pageNumber" : "1"}
 #http://developer.ebay.com/DevZone/finding/CallRef/types/ItemFilterType.html
 itemFilter = [{"name":"MaxPrice", "value":"100", 
                "paramName":"Currency", "paramValue":"EUR"}, 
@@ -19,20 +20,24 @@ itemFilter = [{"name":"MaxPrice", "value":"100",
                "paramName":"Currency", "paramValue":"EUR"}]
 #http://developer.ebay.com/DevZone/finding/CallRef/findItemsByKeywords.html#Request.sortOrder
 sortOrder = "EndTimeSoonest"
-#TODO: These values
-aspectFilter =  [{"key":"value", "key2":"value2"}, {"key3":"value3", "key4":"value4"}]
-domainFilter = [{"key":"value", "key2":"value2"}, {"key3":"value3", "key4":"value4"}]
-outputSelector =["os1", "os2", "os3"]
+aspectFilter =  [{"aspectName":"Color", "aspectValueName":"Black"}, 
+                 {"aspectName":"", "aspectValueName":""}]
+#Multiple domain filters are currently unsupported
+domainFilter = [{"domainName":"Other_MP3_Players"}] 
+#http://developer.ebay.com/DevZone/finding/CallRef/types/OutputSelectorType.html
+outputSelector =["StoreInfo", "SellerInfo", "AspectHistogram"]
 
 
-#TODO: test if results are actually useful
 class TestFindingApi(unittest.TestCase):
     def test_getSearchKeywordsRecommendation(self):
         result = getSearchKeywordsRecommendation(keywords="eipod", encoding=encoding)
+#        print result
         root = etree.fromstring(result)
         ack = root.find("{http://www.ebay.com/marketplace/search/v1/services}ack").text
         self.assertEqual(ack, "Success")
-
+        keyword = root.find("{http://www.ebay.com/marketplace/search/v1/services}keywords").text
+        self.assertEqual(keyword, "ipod")
+        
 
     def test_findItemsByKeywords(self):
         result = findItemsByKeywords(
@@ -41,18 +46,23 @@ class TestFindingApi(unittest.TestCase):
                         buyerPostalCode=buyerPostalCode, \
                         paginationInput=paginationInput, \
                         sortOrder=sortOrder, \
-#                        aspectFilter=aspectFilter, \
-#                        domainFilter=domainFilter, \
+                        aspectFilter=aspectFilter, \
+                        domainFilter=domainFilter, \
                         itemFilter=itemFilter, \
-#                        outputSelector=outputSelector, \
+                        outputSelector=outputSelector, \
                         encoding=encoding)
+#        print result
         root = etree.fromstring(result)
         ack = root.find("{http://www.ebay.com/marketplace/search/v1/services}ack").text
         self.assertEqual(ack, "Success")
+        #Number of Items between 0 and 10, because of paginationInput
+        res_items = root.find("{http://www.ebay.com/marketplace/search/v1/services}searchResult")
+        self.assertTrue(0 <= len(res_items) <= 10)
 
 
     def test_findItemsByCategory(self):
-        result = findItemsByCategory(categoryId=categoryId, \
+        result = findItemsByCategory(
+                        categoryId=categoryId, \
                         affiliate=affiliate, \
                         buyerPostalCode=buyerPostalCode, \
                         sortOrder=sortOrder, \
@@ -62,13 +72,18 @@ class TestFindingApi(unittest.TestCase):
                         itemFilter=itemFilter, \
                         outputSelector=outputSelector, \
                         encoding=encoding)
+#        print result
         root = etree.fromstring(result)
         ack = root.find("{http://www.ebay.com/marketplace/search/v1/services}ack").text
         self.assertEqual(ack, "Success")
+        #Number of Items between 0 and 10, because of paginationInput
+        res_items = root.find("{http://www.ebay.com/marketplace/search/v1/services}searchResult")
+        self.assertTrue(0 <= len(res_items) <= 10)
 
 
     def test_findItemsAdvanced(self):
-        result = findItemsAdvanced(keywords=keywords, \
+        result = findItemsAdvanced(
+                      keywords=keywords, \
                       categoryId=categoryId, \
                       affiliate=affiliate, \
                       buyerPostalCode=buyerPostalCode, \
@@ -79,13 +94,18 @@ class TestFindingApi(unittest.TestCase):
                       itemFilter=itemFilter, \
                       outputSelector=outputSelector, \
                       encoding=encoding)
+#        print result
         root = etree.fromstring(result)
         ack = root.find("{http://www.ebay.com/marketplace/search/v1/services}ack").text
         self.assertEqual(ack, "Success")
+        #Number of Items between 0 and 10, because of paginationInput
+        res_items = root.find("{http://www.ebay.com/marketplace/search/v1/services}searchResult")
+        self.assertTrue(0 <= len(res_items) <= 10)
 
 
     def test_findItemsByProduct(self):
-        result = findItemsByProduct(keywords=keywords, \
+        result = findItemsByProduct(
+                       keywords=keywords, \
                        productId=productId, \
                        affiliate=affiliate, \
                        buyerPostalCode=buyerPostalCode, \
@@ -94,13 +114,15 @@ class TestFindingApi(unittest.TestCase):
                        itemFilter=itemFilter, \
                        outputSelector=outputSelector, \
                        encoding=encoding)
+#        print result
         root = etree.fromstring(result)
         ack = root.find("{http://www.ebay.com/marketplace/search/v1/services}ack").text
-        self.assertEqual(ack, "Failure")
+        self.assertEqual(ack, "Failure") 
 
 
     def test_findItemsIneBayStores(self):
-        result = findItemsIneBayStores(keywords=keywords, \
+        result = findItemsIneBayStores(
+                          keywords=keywords, \
                           storeName=storeName, \
                           affiliate=affiliate, \
                           buyerPostalCode=buyerPostalCode, \
@@ -114,17 +136,22 @@ class TestFindingApi(unittest.TestCase):
         root = etree.fromstring(result)
         ack = root.find("{http://www.ebay.com/marketplace/search/v1/services}ack").text
         self.assertEqual(ack, "Success")
+        #Number of Items between 0 and 10, because of paginationInput
+        res_items = root.find("{http://www.ebay.com/marketplace/search/v1/services}searchResult")
+        self.assertTrue(0 <= len(res_items) <= 10)
 
 
     def test_getHistograms(self):
         result = getHistograms(categoryId=categoryId, encoding=encoding)
+#        print result
         root = etree.fromstring(result)
         ack = root.find("{http://www.ebay.com/marketplace/search/v1/services}ack").text
         self.assertEqual(ack, "Success")
 
+
 if __name__ == '__main__':
-    #Run single test manually
-    t = TestFindingApi("test_findItemsByKeywords")
-    t.test_findItemsByKeywords()
+    #Run single test manually. 
+#    t = TestFindingApi("test_getSearchKeywordsRecommendation")
+#    t.test_getSearchKeywordsRecommendation()
     
-    #unittest.main()
+    unittest.main()
